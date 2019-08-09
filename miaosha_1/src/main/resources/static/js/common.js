f1e8a380581251512a7c9d7f6a75135eb8c8f337
kafun18 <513112374@qq.com>
2019-08-10 00:25:03 +0800

    去掉逗号

--- miaosha_1/src/main/resources/static/js/common.js
+++ miaosha_1/src/main/resources/static/js/common.js
@@ -1,6 +1,6 @@
 //展示loading
 function g_showLoading(){
-	var idx = layer.msg('处理中...', {icon: 16,shade: [0.5, '#f5f5f5'],scrollbar: false,offset: '0px', time:100000}) ;  
+	var idx = layer.msg('处理中...', {icon: 16,shade: [0.5, '#f5f5f5'],scrollbar: false,offset: '0px', time:10000}) ;
 	return idx;
 }
 //salt
